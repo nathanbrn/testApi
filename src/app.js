382e26b8574969e651903c261eba19d73f91a494
Breno Nathanael <breno_nathanael@hotmail.com>
2023-03-19 19:53:19 -0300

    criação de rotas post e get com prisma

--- src/app.js
+++ src/app.js
@@ -1,62 +1,14 @@
 const express = require('express')
 const app = express()
-const { PrismaClient } = require('@prisma/client')
+const router = require('./routes')
 
 const port = 3001
-const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
-
-async function criarUsuario() {
-    const usuario = await prisma.usuario
-    const { id, email } = usuario
-    if(await usuario.findMany()) {
-        console.log('Usuário já existe');
-    } else {
-        usuario.create({
-            data: {
-                id: 45,
-                nome: 'João',
-                email: 'joao@mail.com',
-                password: '123456'
-            }
-        })
-    }
-}
-
-
-// criarUsuario()
-//     .then(() => {
-//     prisma.$disconnect()
-//     })
-//     .catch((err) => {
-//     console.log(err)
-//     prisma.$disconnect()
-//     process.exit(1)
-//     })
-
-async function buscarUsuario() {
-    const usuario = await prisma.usuario.findMany()
-    if(usuario.length > 0)
-        console.log(usuario)
-    else
-        console.log('Não existe usuário cadastrado')
-}
-
-buscarUsuario()
-    .then(() => {
-    prisma.$disconnect()
-    })
-    .catch((err) => {
-    console.log(err)
-    prisma.$disconnect()
-    process.exit(1)
-    })
-
-
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
---
+++ src/routes.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+router.get('/usuarios', (req, res) => {
+    async function buscarTodosOsUsuario() {
+        const usuario = await prisma.usuario.findMany()
+        if(usuario.length > 0)
+            res.status(200).json(usuario)
+        else
+            res.status(404).json({message: 'Nenhum usuário encontrado'})
+    }
+    
+    buscarTodosOsUsuario()
+        .then(() => {
+        prisma.$disconnect()
+        })
+        .catch((err) => {
+        console.log(err)
+        prisma.$disconnect()
+        process.exit(1)
+        })
+})
+
+router.post('/usuarios', async (req, res) => {
+    const { id, email, name, password } = req.body
+    try {
+        const usuario = await prisma.usuario.create({
+            data: {
+                id,
+                email,
+                name,
+                password
+            }
+        })
+        res.status(201).json(usuario)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'Erro ao criar usuário'})
+    }
+})
+
+module.exports = router
