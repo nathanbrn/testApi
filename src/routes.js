59541015a18d7b71ba9554a2b78e93fc85b111ce
Breno Nathanael <breno_nathanael@hotmail.com>
2023-03-21 15:41:54 -0300

    criado rotas de atualização de usuario e cliente

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,48 @@ router.post('/clientes', clienteService.criarCliente)
 
 router.get('/clientes/:id', clienteService.buscarClientePorId)
 
+router.patch('/clientes/:id', async (req, res) => {
+    const { id } = req.params
+    const { email, name, vendedorId } = req.body
+    try {
+        const cliente = await prisma.cliente.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                email,
+                name,
+                vendedorId
+            }
+        })
+        res.status(201).json(cliente)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Erro ao atualizar cliente' })
+    }
+})
+
+router.patch('/usuarios/:id', async (req, res) => {
+    const { id } = req.params
+    const { email, name, password } = req.body
+    try {
+        const usuario = await prisma.usuario.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                email,
+                name,
+                password
+            }
+        })
+        res.status(201).json(usuario)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'Erro ao atualizar cliente' })
+    }
+})
+
 router.delete('/clientes/:id', clienteService.deletarCliente)
 
 router.delete('/usuarios/:id', usuarioService.deletarUsuario)
